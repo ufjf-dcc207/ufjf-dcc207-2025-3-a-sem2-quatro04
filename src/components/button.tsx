4d9d2e4d2bab24b1df1e886859ddaea1c29b37a5
Antony Kleverton <antonykleverton.guedes@estudante.ufjf.br>
2025-12-08 01:21:28 -0300

    Inicia implementação de alteração de estado do botão

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type ButtonProps = {
   texto: string;
   status?: 'tomado' | 'pendente';
@@ -5,16 +7,30 @@ type ButtonProps = {
 
 export default function Button({ texto, status = 'pendente' }: ButtonProps) {
   
-  if (status === 'tomado') {
+  const [isClicked, setIsClicked] = useState(status);
+
+  function toNext(){
+      switch (isClicked){
+        case "pendente":
+            setIsClicked("tomado");
+            break;
+        case "tomado":
+            setIsClicked("pendente");
+            break;
+      }
+  }
+
+  if (isClicked === 'tomado') {
     return (
-      <button className="bg-[#12b36d] text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-[#10a564] transition">
+      <button onClick={toNext} className="bg-[#12b36d] text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-[#10a564] transition">
         {texto}
       </button>
     );
+  } else {
+   return (
+     <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 transition">
+       {texto}
+     </button>
+   );
   }
-  return (
-    <button className="bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-blue-700 transition">
-      {texto}
-    </button>
-  );
 }
