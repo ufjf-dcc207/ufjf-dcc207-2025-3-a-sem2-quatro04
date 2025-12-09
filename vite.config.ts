d0725e56102fce854cb3d1cd864f62de7ae6b99a
Antony Kleverton <antonykleverton.guedes@estudante.ufjf.br>
2025-12-09 19:55:26 -0300

    Faz últimas alterações para possibilitar deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ufjf-dcc207-2025-3-a-sem01-404/",
+  base: "/ufjf-dcc207-2025-3-a-sem2-quatro04/",
 })
